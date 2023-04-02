97d655e7c74219fe408f741edcd7ecbda4438afb
Alphywilson07 <127036086+Alphywilson07@users.noreply.github.com>
2023-04-02 22:57:52 +0530

    Add files via upload

---
+++ ASSIGNMENT 5 Q3.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self):
+        self.__name = None
+        self.__rollNumber = None
+    
+    def getName(self):
+        return self.__name
+    
+    def setName(self, name):
+        self.__name = name
+    
+    def getRollNumber(self):
+        return self.__rollNumber
+    
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber
+s = Student()
+s.setName("John Doe")
+s.setRollNumber(12345)
+print(s.getName())  # Output: John Doe
+print(s.getRollNumber())  # Output: 12345
