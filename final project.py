9e9622c18211c6914f2373d62916ceb0a6264d13
Alphywilson07 <127036086+Alphywilson07@users.noreply.github.com>
2023-04-09 22:39:33 +0530

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,29 @@
+# User Registration:
+# Users will be required to register on the app by providing their Full Name, Phone Number, Email, Address, and Password. Upon successful registration, the user will be redirected to the login page.
+
+# User Login:
+# Registered users will log in to the app using their email and password.
+
+# Place New Order:
+# The user can select food items from a list of available items. Each item will have its name, quantity, price, and an option to add it to the cart. Once the user has selected all the items they wish to order, they can proceed to the cart and review their order.
+
+# Order History:
+# Users will be able to view a list of all their previous orders, along with the order details and total amount paid.
+
+# Update Profile:
+# Users should be able to update their profile information, including their name, email, phone number, and address.
+
+# Admin Login:
+# The admin will be required to log in to the app using their username and password.
+
+# Add New Food Items:
+# The admin can add new food items to the menu by providing details such as FoodID, Name, Quantity, Price, Discount, and Stock.
+
+# Edit Food Items:
+# The admin can modify the details of existing food items by searching for the item using its FoodID and then updating the relevant information.
+
+# View Food Items:
+# The admin can view a list of all the food items available in the restaurant.
+
+# Remove Food Items:
+# The admin can remove any food item from the menu by searching for the item using its FoodID and then deleting it.
